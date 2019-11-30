1ec57dd8f565a18d55cb06a4c25fe0fb23bc9027
Roshan <shrestharoshan776@gmail.com>
2019-11-30 22:40:18 +0545

    login done

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
+import dao.LoginDao;
 import dao.RegisterDao;
+import daoImpl.LoginDaoImpl;
 import daoImpl.RegisterDaoImpl;
 
 import java.rmi.registry.LocateRegistry;
@@ -9,8 +11,10 @@ public class Server {
     public static void main(String[] args) {
         try {
             RegisterDao rd= new RegisterDaoImpl();
+            LoginDao ld = new LoginDaoImpl();
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind("Register", rd);
+            registry.rebind("Login",ld);
             System.out.print("Server Started");
         } catch (Exception ex) {
             ex.printStackTrace();
--- src/daoImpl/LoginDaoImpl.java
+++ src/daoImpl/LoginDaoImpl.java
@@ -8,6 +8,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginDaoImpl extends UnicastRemoteObject implements LoginDao{
@@ -15,18 +16,20 @@ public class LoginDaoImpl extends UnicastRemoteObject implements LoginDao{
     public LoginDaoImpl() throws RemoteException{
         super();
     }
+
     @Override
     public Boolean checkUser(String user_id, String user_password) throws RemoteException{
         try {
             System.out.println("Login method called");
-            String sql ="SELECT user_id, user_password form user where user_id =? and user_password=?";
-
-            PreparedStatement ps = cn.prepareStatement((sql));
-
+            String sql ="SELECT * FROM user WHERE user_id =? and user_password=?";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1,user_id);
+            ps.setString(2,user_password);
+            ResultSet rs = ps.executeQuery();
+            return  rs.next();
         } catch (Exception e) {
-
             System.out.println("exception"+e);
+            return null;
         }
-        return null;
     }
 }
