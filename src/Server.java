d5f1b1145188a51d8a1014ded42f35af4208938a
Jenish Tamrakar <jenishtamrakar11@gmail.com>
2019-12-03 21:02:05 +0545

    Code to add Student records from admin side

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,9 @@
 import dao.LoginDao;
 import dao.RegisterDao;
+import dao.StudentDao;
 import daoImpl.LoginDaoImpl;
 import daoImpl.RegisterDaoImpl;
+import daoImpl.StudentDaoImpl;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -12,9 +14,11 @@ public class Server {
         try {
             RegisterDao rd= new RegisterDaoImpl();
             LoginDao ld = new LoginDaoImpl();
+            StudentDao sd = new StudentDaoImpl();
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind("Register", rd);
             registry.rebind("Login",ld);
+            registry.rebind("Student", sd);
             System.out.print("Server Started");
         } catch (Exception ex) {
             ex.printStackTrace();
