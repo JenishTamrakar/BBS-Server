9c3812a283abc4921bf1d69137e573bbed035d23
Roshan <shrestharoshan776@gmail.com>
2019-12-18 09:59:12 +0545

    login on user role fixed

--- src/daoImpl/LoginDaoImpl.java
+++ src/daoImpl/LoginDaoImpl.java
@@ -1,9 +1,11 @@
 package daoImpl;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.sun.rowset.CachedRowSetImpl;
 import dao.LoginDao;
 import utils.DbConnection;
 
+import javax.sql.rowset.CachedRowSet;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.Connection;
@@ -18,19 +20,22 @@ public class LoginDaoImpl extends UnicastRemoteObject implements LoginDao{
     }
 
     @Override
-    public Boolean checkUser(String user_id, String user_password) throws RemoteException{
+    public ResultSet checkUser(String user_id,String user_password) throws RemoteException{
         try {
             System.out.println("Login method called");
-            String sql ="SELECT * FROM user WHERE user_id =? and user_password=?";
+            String sql ="SELECT * from user";
             PreparedStatement ps = cn.prepareStatement(sql);
-            ps.setString(1,user_id);
-            ps.setString(2,user_password);
-            ResultSet rs = ps.executeQuery();
-            return  rs.next();
 
-        } catch (Exception e) {
-            System.out.println("exception"+e);
-            return null;
+
+
+            ResultSet rs = ps.executeQuery();
+            CachedRowSetImpl crs= new CachedRowSetImpl();
+            crs.populate(rs);
+            return crs;
+
+    } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
