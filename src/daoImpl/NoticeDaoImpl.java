80412a058db4c0c4700bef62fdaed0301c325e01
Jenish Tamrakar <jenishtamrakar11@gmail.com>
2019-12-18 20:51:55 +0545

    Notice displayed in table and CRUD operation

--- src/daoImpl/NoticeDaoImpl.java
+++ src/daoImpl/NoticeDaoImpl.java
@@ -1,6 +1,7 @@
 package daoImpl;
 
 import bll.Notice;
+import com.sun.rowset.CachedRowSetImpl;
 import dao.NoticeDao;
 import utils.DbConnection;
 
@@ -8,6 +9,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -18,9 +20,9 @@ public NoticeDaoImpl()throws RemoteException{
 }
     @Override
     public void addNotice(Notice n) throws RemoteException {
-        System.out.println(n.getNoticeTitle());
-        System.out.println(n.getNoticeDate());
-        System.out.println(n.getNoticeDescription());
+//        System.out.println(n.getNoticeTitle());
+//        System.out.println(n.getNoticeDate());
+//        System.out.println(n.getNoticeDescription());
         try {
             String sql ="INSERT INTO notice(notice_title, notice_description, notice_date) VALUES(?,?,?)";
             PreparedStatement ps = cn.prepareStatement(sql);
@@ -29,11 +31,62 @@ public NoticeDaoImpl()throws RemoteException{
             ps.setString(3,n.getNoticeDate());
 
             ps.executeUpdate();
-            System.out.println("hello");
+//            System.out.println("hello");
         }catch (SQLException e){
             System.out.println(e);
         }
 
+    }
+
+    @Override
+    public void updateNotice(Notice n)throws RemoteException
+    {
+        try
+        {
+            String sql = "UPDATE notice set notice_title = ?, notice_description = ?, notice_date= ? where notice_id = ?";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, n.getNoticeTitle());
+            ps.setString(2, n.getNoticeDescription());
+            ps.setString(3, n.getNoticeDate());
+            ps.setString(4, n.getNoticeID());
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void deleteNotice(Notice n)throws RemoteException
+    {
+        try
+        {
+            String sql = "DELETE from notice where notice_id = ?";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, n.getNoticeID());
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public ResultSet getNoticeDetails() throws RemoteException{
+        try
+        {
+            ResultSet rs = cn.createStatement().executeQuery("select notice_id, notice_title, notice_description, notice_date from notice");
+            CachedRowSetImpl crsi = new CachedRowSetImpl();
+            crsi.populate(rs);
+            return crsi;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
     }
 }
 
