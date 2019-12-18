949e515e4bcae51df34ab94be954bfdeb93ea4ff
Jenish Tamrakar <jenishtamrakar11@gmail.com>
2019-12-18 18:44:39 +0545

    Assignment displayed in table and CRUD operation

--- src/daoImpl/AssignmentDaoImpl.java
+++ src/daoImpl/AssignmentDaoImpl.java
@@ -1,15 +1,13 @@
 package daoImpl;
 
 import bll.Assignment;
+import com.sun.rowset.CachedRowSetImpl;
 import dao.AssignmentDao;
 import utils.DbConnection;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AssignmentDaoImpl extends UnicastRemoteObject implements AssignmentDao
 {
@@ -37,5 +35,58 @@ public class AssignmentDaoImpl extends UnicastRemoteObject implements Assignment
         {
             System.out.println(e);
         }
+    }
+
+    @Override
+    public void updateAssDet(Assignment as)throws RemoteException
+    {
+        try
+        {
+            String sql = "UPDATE assignment set assignment_title = ?, assignment_level = ?, assignment_course = ?, assignment_unit = ?, assignment_deadline_date= ? where assignment_id = ?";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, as.getAss_title());
+            ps.setString(2, as.getAss_level());
+            ps.setString(3, as.getAss_course());
+            ps.setString(4, as.getAss_unit());
+            ps.setString(5, as.getAss_date().toString());
+            ps.setString(6, as.getAss_ID());
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void deleteAssDet(Assignment as)throws RemoteException
+    {
+        try
+        {
+            String sql = "DELETE from assignment where assignment_id = ?";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, as.getAss_ID());
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public ResultSet getAssignDetails() throws RemoteException{
+        try
+        {
+            ResultSet rs = cn.createStatement().executeQuery("select assignment_id, assignment_title, assignment_level, assignment_course, assignment_unit, assignment_deadline_date from assignment");
+            CachedRowSetImpl crsi = new CachedRowSetImpl();
+            crsi.populate(rs);
+            return crsi;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
     }
 }
