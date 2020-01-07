bcabbb6ddb10832f05f87b0957908b1e8e2eaabc
Roshan Shrestha <44658790+roshanoob@users.noreply.github.com>
2020-01-07 14:10:47 +0545

    update

--- src/bll/Faculty.java
+++ src/bll/Faculty.java
@@ -81,7 +81,7 @@ public class Faculty implements Serializable
         this.faculty_SN = faculty_SN;
         this.faculty_ID = faculty_ID;
         this.name = name;
-        //this.pass = pass;
+
         this.course = course;
         this.email = email;
     }
